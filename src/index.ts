0816c2c7fd32a41a1d84b3b8afb5cd099803a148
Subhajit Sahu <wolfram77@gmail.com>
2022-10-04 21:15:32 +0530

    :mango: remove url suffix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-function main() {
+import {Application, DefaultTheme, Reflection} from "typedoc";
+
+
+
+
+// Modify the default theme.
+class ExtraTypedocTheme extends DefaultTheme {
+  // Remove url suffix (function-1.html → function.html).
+  static getUrl(reflection: Reflection, relative?: Reflection, separator?: string): string {
+    var url = DefaultTheme.getUrl(reflection, relative, separator);
+    return url.replace(/\-\d+(\.\w+)?/, "$1");
+  }
 }
-main();
+
+
+// Define the theme.
+export function load(app: Application) {
+  app.renderer.defineTheme("extra-typedoc-theme", ExtraTypedocTheme);
+}
